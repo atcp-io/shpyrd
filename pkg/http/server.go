c0ff662dbeef63f614f7196229baac704948cef2
Patrick Negri <patrick@iugu.com>
2023-07-21 22:20:50 -0300

    feat: added helm chart listing support

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -6,13 +6,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"shpyrd/pkg/kube"
+	"shpyrd/pkg/k8shp"
+
+	"helm.sh/helm/v3/pkg/action"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func Hello() string {
@@ -22,6 +27,13 @@ func Hello() string {
 func pong(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
+	})
+}
+
+func install_helm(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "install_helm",
 	})
 }
 
@@ -41,7 +53,7 @@ func defaultHandler(c *gin.Context) {
 
 	namespaces, err := ListNamespaces(kubeClient)
 	if err != nil {
-		fmt.Println(err.Error)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
@@ -90,10 +102,26 @@ func New() {
 
 var kubeClient *kubernetes.Clientset
 
+func getActionConfig(namespace string, config *rest.Config) (*action.Configuration, error) {
+	actionConfig := new(action.Configuration)
+	var kubeConfig *genericclioptions.ConfigFlags
+	// Create the ConfigFlags struct instance with initialized values from ServiceAccount
+	kubeConfig = genericclioptions.NewConfigFlags(false)
+	kubeConfig.APIServer = &config.Host
+	kubeConfig.BearerToken = &config.BearerToken
+	kubeConfig.CAFile = &config.CAFile
+	kubeConfig.Namespace = &namespace
+	if err := actionConfig.Init(kubeConfig, namespace, os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		return nil, err
+	}
+	return actionConfig, nil
+}
+
 func Run() {
+	fmt.Println("Starting...")
 	log.SetLevel(log.DebugLevel)
 
-	kubeConfig, err := kube.AutoDetectConfig()
+	kubeConfig, err := k8shp.AutoDetectConfig()
 	if err != nil {
 		log.Debug(err)
 	}
@@ -104,9 +132,20 @@ func Run() {
 		kubeConfig.Host = "https://kubernetes.default.svc.cluster.local:54724"
 	}
 	log.Debug("Kubernetes Host: ", kubeConfig.Host)
-	kubeClient, err = kube.KubernetesClient(kubeConfig)
+	kubeClient, err = k8shp.KubernetesClient(kubeConfig)
 	if err != nil {
 		log.Debug(err)
+	}
+
+	actionConfig, err := getActionConfig("", kubeConfig)
+	listAction := action.NewList(actionConfig)
+	releases, err := listAction.Run()
+	if err != nil {
+		fmt.Println("ERROR - HELM")
+		log.Println(err)
+	}
+	for _, release := range releases {
+		log.Println("Release: " + release.Name + " Status: " + release.Info.Status.String())
 	}
 
 	fmt.Println(gin.Version)
